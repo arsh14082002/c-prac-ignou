97b15844ee9905b7f477887e58bc0c15f4c15984
arsh14082002 <thesiddiqui7@gmail.com>
2024-12-04 00:31:48 +0530

    implement doublylinked list more efficiently

--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -7,7 +7,7 @@ struct Node{
     struct Node *prev;
 };
 
-void createDoublyLinkedList(struct Node **head, int val){
+void insertAtEnd(struct Node **head, int val){
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = val;
     newNode->next = NULL;
@@ -25,30 +25,85 @@ void createDoublyLinkedList(struct Node **head, int val){
     }
 }
 
-void deleteDoublyLinkedList(struct Node **head, int val){
-    struct Node *temp = *head;
-    struct Node *prev = NULL;
-    if(temp != NULL && temp->data == val){
-        *head = temp->next;
-        free(temp);
-        return;
+void insertAtStart(struct Node **head, int val){
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    if(*head == NULL){
+        *head = newNode;
     }
-    while(temp != NULL && temp->data != val){
-        prev = temp;
-        temp = temp->next;
+    else{
+        newNode->next = *head;
+        (*head)->prev = newNode;
+        *head = newNode;
     }
-    if(temp == NULL){
-        return;
-    }
-    prev->next = temp->next;
-    free(temp);
 }
 
-void searchDoublyLinkedList(struct Node *head, int val){
+void insertAtPosition(struct Node **head, int pos, int val){
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    if(*head == NULL){
+        *head = newNode;
+    }
+    else{
+        struct Node *temp = *head;
+        int count = 1;
+        while(temp->next != NULL && count < pos){
+            temp = temp->next;
+            count++;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        newNode->prev = temp;
+        newNode->next->prev = newNode;
+    }
+}
+
+void deleteAtStart(struct Node **head){
+    struct Node *temp = *head;
+    if(temp != NULL){
+        *head = temp->next;
+        free(temp);
+    }
+}
+
+void deleteAtEnd(struct Node **head){
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
+    if(temp != NULL){
+        while(temp->next != NULL){
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = NULL;
+        free(temp);
+    }
+}
+
+void deleteAtPosition(struct Node **head, int pos){
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
+    int count = 1;
+    if(temp != NULL){
+        while(temp->next != NULL && count < pos){
+            prev = temp;
+            temp = temp->next;
+            count++;
+        }
+        prev->next = temp->next;
+        free(temp);
+    }
+}
+
+void searchNode(struct Node *head, int val){
     struct Node *temp = head;
     while(temp != NULL){
         if(temp->data == val){
             printf("Found\n");
+            printf("Data: %d\n", temp->data);
             return;
         }
         temp = temp->next;
@@ -56,7 +111,30 @@ void searchDoublyLinkedList(struct Node *head, int val){
     printf("Not Found\n");
 }
 
-void showDoublyLinkedList(struct Node *head){
+void reverse(struct Node **head){
+    struct Node *prev = NULL;
+    struct Node *current = *head;
+    struct Node *next = NULL;
+    while(current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+
+void size(struct Node *head){
+    int count = 0;
+    struct Node *temp = head;
+    while(temp != NULL){
+        count++;
+        temp = temp->next;
+    }
+    printf("Size: %d\n", count);
+}
+
+void showNode(struct Node *head){
     struct Node *temp = head;
     printf("HEAD");
     while(temp != NULL){
@@ -67,14 +145,69 @@ void showDoublyLinkedList(struct Node *head){
 }
 
 int main(){
+    int ch, val, pos=-1;
     struct Node *head = NULL;
-    createDoublyLinkedList(&head, 10);
-    createDoublyLinkedList(&head, 20);
-    createDoublyLinkedList(&head, 30);
-    createDoublyLinkedList(&head, 40);
-    createDoublyLinkedList(&head, 50);
-    deleteDoublyLinkedList(&head, 20);
-    searchDoublyLinkedList(head, 30);
-    showDoublyLinkedList(head);
+    do{
+        printf("\n1. Insert \n2. Delete \n3. Traverse \n4. Size \n5. Reverse \n6. Search \n7. Exit \nEnter your choice: ");
+        scanf("%d", &ch);
+        switch (ch){
+            case 1:
+                printf("\n1. Insert at end \n2. Insert at beginning \n3. Insert at position \nEnter your choice: ");
+                scanf("%d", &ch);
+                switch (ch){
+                    case 1:
+                        printf("Enter value: ");
+                        scanf("%d", &val);
+                        insertAtEnd(&head, val);
+                        break;
+                    case 2:
+                        printf("Enter value: ");
+                        scanf("%d", &val);
+                        insertAtStart(&head, val);
+                        break;
+                    case 3:
+                        printf("Enter value: ");
+                        scanf("%d", &val);
+                        printf("Enter position: ");
+                        scanf("%d", &pos);
+                        insertAtPosition(&head, pos, val);
+                        break;
+                }
+                break;
+            case 2:
+                printf("\n1. Delete at end \n2. Delete at beginning \n3. Delete at position \nEnter your choice: ");
+                scanf("%d", &ch);
+                switch (ch){
+                    case 1: 
+                        deleteAtEnd(&head);
+                        break;
+                    case 2:
+                        deleteAtStart(&head);
+                        break;
+                    case 3:
+                        printf("Enter position: ");
+                        scanf("%d", &pos);
+                        deleteAtPosition(&head, pos);
+                        break;
+                }
+                break;
+            case 3:
+                showNode(head);
+                break;
+            case 4:
+                size(head);
+                break;
+            case 5:
+                reverse(&head);
+                break;
+            case 6:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                searchNode(head, val);
+                break;
+            case 7:
+                break;
+        }
+    }while(ch != 7);
     return 0;
 }
--- linkedList.c
+++ linkedList.c
@@ -7,11 +7,10 @@ struct Node
     struct Node *next;
 };
 
-void createNode(struct Node **head, int val){
+void insertAtEnd(struct Node **head, int val){
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = val;
     newNode->next = NULL;
-
     if(*head == NULL){
         *head = newNode;
     }
@@ -24,23 +23,99 @@ void createNode(struct Node **head, int val){
     }
 }
 
-void deleteNode(struct Node **head, int val){
+void insertAtStart(struct Node **head, int val){
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    if(*head == NULL){
+        *head = newNode;
+    }
+    else{
+        newNode->next = *head;
+        *head = newNode;
+    }
+}
+
+void insertAtPosition(struct Node **head, int val, int pos){
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    if(*head == NULL){
+        *head = newNode;
+    }
+    else{
+        struct Node *temp = *head;
+        int count = 1;
+        while(temp->next != NULL && count < pos-1){
+            temp = temp->next;
+            count++;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void deleteAtEnd(struct Node **head){
     struct Node *temp = *head;
     struct Node *prev = NULL;
-    if(temp != NULL && temp->data == val){
-        *head = temp->next;
-        free(temp);
-        return;
-    }
-    while(temp != NULL && temp->data != val){
+    while(temp->next != NULL){
         prev = temp;
         temp = temp->next;
     }
-    if(temp == NULL){
-        return;
+    prev->next = NULL;
+    free(temp);
+}
+
+void deleteAtStart(struct Node **head){
+    struct Node *temp = *head;
+    *head = temp->next;
+    free(temp);
+}
+
+void deleteAtPosition(struct Node **head, int pos){
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
+    int count = 1;
+    while(temp->next != NULL && count < pos){
+        prev = temp;
+        temp = temp->next;
+        count++;
     }
     prev->next = temp->next;
     free(temp);
+}
+
+void showNode(struct Node *head){
+    struct Node *temp = head;
+    printf("HEAD");
+    while(temp != NULL){
+        printf(" -> %d ", temp->data);
+        temp = temp->next;
+    }
+    printf("-> NULL\n");
+}
+
+void size(struct Node *head){
+    int count = 0;
+    struct Node *temp = head;
+    while(temp != NULL){
+        count++;
+        temp = temp->next;
+    }
+    printf("Size: %d\n", count);
+}
+
+void reverse(struct Node **head){
+    struct Node *prev = NULL;
+    struct Node *current = *head;
+    struct Node *next = NULL;
+    while(current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
 }
 
 void searchNode(struct Node *head, int val){
@@ -55,27 +130,111 @@ void searchNode(struct Node *head, int val){
     printf("Not Found\n");
 }
 
-void showNode(struct Node *head){
-    struct Node *temp = head;
-    printf("HEAD");
-    while(temp != NULL){
-        printf(" -> %d ", temp->data);
-        temp = temp->next;
-    }
-    printf("-> NULL\n");
-}
-
-
 int main(){
-    struct Node *head = NULL;
-    createNode(&head, 10);
-    createNode(&head, 20);
-    createNode(&head, 30);
-    createNode(&head, 40);
-    createNode(&head, 50);
-    deleteNode(&head, 20);
-    searchNode(head, 30);
+    int ch, val;
+    struct Node *head = NULL;  // Use struct Node* for the head of the list
+    
+    do {
+        // Display the main menu
+        printf("\n1. Insert \n2. Delete \n3. Traverse \n4. Size \n5. Reverse \n6. Search \n7. Exit \nEnter your choice: ");
+        scanf("%d", &ch);
 
-    showNode(head);
-    return 0;
+        switch (ch){
+            case 1:
+                // Insert submenu
+                printf("\n1. Insert at end \n2. Insert at beginning \n3. Insert at position \nEnter your choice: ");
+                scanf("%d", &ch);
+                switch (ch){
+                    case 1:
+                        // insert at end
+                        printf("Enter value: ");
+                        scanf("%d", &val);
+                        insertAtEnd(&head, val);
+                        break;
+
+                    case 2:
+                        // insert at beginning
+                        printf("Enter value: ");
+                        scanf("%d", &val);
+                        insertAtStart(&head, val);
+                        break;
+
+                    case 3:
+                        // insert at position
+                        printf("Enter value: ");
+                        scanf("%d", &val);
+                        int pos;
+                        printf("Enter position: ");
+                        scanf("%d", &pos);
+                        insertAtPosition(&head, val, pos);
+                        break;
+
+                    default:
+                        printf("Invalid choice for insertion.\n");
+                        break;
+                }
+                break;
+
+            case 2:
+                // Delete submenu
+                printf("\n1. Delete at end \n2. Delete at beginning \n3. Delete at position \nEnter your choice: ");
+                scanf("%d", &ch);
+                switch (ch){
+                    case 1:
+                        // delete at end
+                        deleteAtEnd(&head);
+                        break;
+
+                    case 2:
+                        // delete at beginning
+                        deleteAtStart(&head);
+                        break;
+
+                    case 3:
+                        // delete at position
+                        int pos;
+                        printf("Enter position: ");
+                        scanf("%d", &pos);
+                        deleteAtPosition(&head, pos);
+                        break;
+
+                    default:
+                        printf("Invalid choice for deletion.\n");
+                        break;
+                }
+                break;
+
+            case 3:
+                // Traverse the list
+                showNode(head);
+                break;
+
+            case 4:
+                // Display size of the list
+                size(head);
+                break;
+
+            case 5:
+                // Reverse the list
+                reverse(&head);
+                break;
+
+            case 6:
+                // Search for a value
+                printf("Enter value: ");
+                scanf("%d", &val);
+                searchNode(head, val);
+                break;
+
+            case 7:
+                // Exit
+                printf("Exiting the program.\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Invalid choice, please try again.\n");
+                break;
+        }
+    } while(ch != 7);
 }
